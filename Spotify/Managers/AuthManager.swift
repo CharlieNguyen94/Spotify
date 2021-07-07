679eccc1aab919fca9931559b14ba4a30874ea65
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-07-07 15:00:40 +0100

    Sign in button on welcome screen.

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,6 +10,11 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientId = "bacf0f4c18f148a89a23a05f54718918"
+        static let clientSecret = "8cca2181e073418fb3d9987c43e43228"
+    }
+    
     private init() {}
     
     
