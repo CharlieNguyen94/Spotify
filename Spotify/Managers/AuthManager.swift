f9918ac281eef58740bbb1337c84997c9108559a
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-07-07 15:59:44 +0100

    auth token implementation.

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -11,11 +11,20 @@ final class AuthManager {
     static let shared = AuthManager()
     
     struct Constants {
-        static let clientId = "bacf0f4c18f148a89a23a05f54718918"
+        static let clientID = "bacf0f4c18f148a89a23a05f54718918"
         static let clientSecret = "8cca2181e073418fb3d9987c43e43228"
     }
     
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
     
     
     var isSignedIn: Bool {
@@ -36,6 +45,21 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+       // Get token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    private func cacheToken() {
+        
     }
     
 }
