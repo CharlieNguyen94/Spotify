a4e79279abc71c90e93da4b37fb41ac1183fa308
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-07-23 12:40:35 +0100

    Implemented and fixed design of welcome screen.

--- Spotify/Controllers/Other/WelcomeViewController.swift
+++ Spotify/Controllers/Other/WelcomeViewController.swift
@@ -13,8 +13,41 @@ class WelcomeViewController: UIViewController {
         let button = UIButton()
         button.backgroundColor = .white
         button.setTitle("Sign in with spotify", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGreen
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = 20
         return button
+    }()
+    
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "albums_background")
+        return imageView
+    }()
+    
+    private let overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.7
+        return view
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        label.text = "Listen to Millions\nof Song on\nthe go."
+        return label
     }()
 
     override func viewDidLoad() {
@@ -22,17 +55,26 @@ class WelcomeViewController: UIViewController {
 
         title = "Spotify"
         view.backgroundColor = .systemGreen
+        view.addSubview(backgroundImageView)
+        view.addSubview(overlayView)
         view.addSubview(signInButton)
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        view.addSubview(label)
+        view.addSubview(logoImageView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        backgroundImageView.frame = view.bounds
+        overlayView.frame = view.bounds
         signInButton.frame = CGRect(
             x: 20,
-            y: view.height - 50 - view.safeAreaInsets.bottom,
+            y: view.height - 80 - view.safeAreaInsets.bottom,
             width: view.width - 40,
             height: 50)
+        
+        logoImageView.frame = CGRect(x: (view.width - 120) / 2, y: (view.height - 350) / 2, width: 120, height: 120)
+        label.frame = CGRect(x: 30, y: logoImageView.bottom + 30, width: view.width - 60, height: 150)
     }
     
     @objc func didTapSignIn() {
